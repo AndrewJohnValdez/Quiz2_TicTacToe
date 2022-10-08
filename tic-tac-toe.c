9b4f3a4905e522f753a18b28098c242bdeff17ab
AndrewJohnValdez <andrewjohnv@cpp.edu>
2022-10-07 22:01:52 -0700

    planning setup of tictactoe game

---
+++ tic-tac-toe.c
@@ -0,0 +1,22 @@
+//Tic Tac Toe game between 2 players or player vs computer
+/*
+Potential requirements:
+randomizer to imitate computer
+loop back and forth from player to player
+create board(2d Array?)
+check if last choice is a winning choice
+*/
+
+//int main() {}
+/*
+    //prompt user for game they wish to play
+    //write code here
+
+
+    //read information from console
+    //write code here
+
+
+    //set up and print board 
+    //write code here
+*/
